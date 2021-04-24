9aab2079ede72e19961a2e48b4ab9f6f9bde0ac2
Stepan Bogdanov <bogstepan@gmail.com>
2021-04-24 13:00:02 +0300

    0.4. BufferedOutputStream [#503126]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -1,7 +1,6 @@
 package ru.job4j.io;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,8 +16,22 @@ public class LogFilter {
         return log;
     }
 
+    public static void save(List<String> log) {
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("log_result.txt")))) {
+            for (String s : log) {
+                out.write(s);
+                out.write(System.lineSeparator());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         List<String> log = filter("log.txt");
         System.out.println(log);
+        save(log);
     }
 }

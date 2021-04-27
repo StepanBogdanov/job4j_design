147676871f5703b346519391b4620767f399a1ef
Stepan Bogdanov <bogstepan@gmail.com>
2021-04-27 06:31:28 +0300

    1. Читаем файл конфигурации [#503130]

---
+++ src/test/java/ru/job4j/io/ConfigTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class ConfigTest {
+
+    @Test
+    public void whenPairWithoutComment() {
+        String path = "pair_without_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"),is("Petr Arsentev"));
+    }
+
+    @Test
+    public void whenPairWithComment() {
+        String path = "pair_with_comment.properties";
+        Config config = new Config(path);
+        config.load();
+        assertThat(config.value("name"),is("Petr Arsentev"));
+    }
+
+}

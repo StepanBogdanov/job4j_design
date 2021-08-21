3f587c6449a786174f17b5cbc1b52ed410a5b00d
Stepan Bogdanov <bogstepan@gmail.com>
2021-08-21 17:39:46 +0300

    0. Что такое база данных. [#6860 #192145]

--- requests.sql
+++ requests.sql
@@ -1,3 +1,9 @@
+create table goods(
+    id seial primary key,
+    name varchar(255),
+    price money,
+    count int
+);
 insert into goods(name, price, count) values ('Bread', 45.0, 25);
 update goods set price = 46.0;
 delete from goods;

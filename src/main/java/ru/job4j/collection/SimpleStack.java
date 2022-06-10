e3370e654f11e1d80ea2efc3a2daf0db771b3012
Stepan Bogdanov <bogstepan@gmail.com>
2022-06-10 11:43:08 +0300

    5. Очередь на двух стеках [#160]

--- src/main/java/ru/job4j/collection/SimpleStack.java
+++ src/main/java/ru/job4j/collection/SimpleStack.java
@@ -11,4 +11,8 @@ public class SimpleStack<T> {
     public void push(T value) {
         linked.addFirst(value);
     }
+
+    public boolean hasNext() {
+        return linked.iterator().hasNext();
+    }
 }

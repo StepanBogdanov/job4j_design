b08337954bfb6dfca3643853fb892f649bf682f0
Stepan Bogdanov <bogstepan@gmail.com>
2022-08-05 13:15:54 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ src/main/java/ru/job4j/psql/create.sql
@@ -0,0 +1,50 @@
+create table roles(
+    id serial primary key,
+    name text
+);
+
+create table users(
+    id serial primary key,
+    name text,
+    role_id int references roles(id)
+);
+
+create table rules(
+    id serial primary key,
+    rule text
+);
+
+create table role_rules(
+    id serial primary key,
+    role_id int references roles(id),
+    rule_id int references rules(id)
+);
+
+create table categories(
+    id serial primary key,
+    category int
+);
+
+create table states(
+    id serial primary key,
+    state text
+);
+
+create table items(
+    id serial primary key,
+    name text,
+    category_id int references categories(id),
+    state_id int references states(id)
+);
+
+create table comments(
+    id serial primary key,
+    comment text,
+    item_id int references items(id)
+);
+
+create table attachs(
+    id serial primary key,
+    path text,
+    item_id int references items(id)
+);
---
+++ src/main/java/ru/job4j/psql/insert.sql
@@ -0,0 +1,38 @@
+insert into roles(name) values ('role1');
+insert into roles(name) values ('role2');
+
+insert into users(name, role_id) values ('name1', 1);
+insert into users(name, role_id) values ('name2', 1);
+insert into users(name, role_id) values ('name3', 2);
+
+insert into rules(rule) values ('rule1');
+insert into rules(rule) values ('rule2');
+insert into rules(rule) values ('rule3');
+
+insert into role_rules(role_id, rule_id) values (1, 1);
+insert into role_rules(role_id, rule_id) values (1, 2);
+insert into role_rules(role_id, rule_id) values (2, 1);
+insert into role_rules(role_id, rule_id) values (2, 3);
+
+insert into categories(category) values (1);
+insert into categories(category) values (2);
+insert into categories(category) values (3);
+
+insert into states(state) values ('state1');
+insert into states(state) values ('state2');
+
+insert into items(name, category_id, state_id) values ('name1', 1, 1);
+insert into items(name, category_id, state_id) values ('name2', 1, 2);
+insert into items(name, category_id, state_id) values ('name3', 2, 2);
+insert into items(name, category_id, state_id) values ('name4', 3, 1);
+
+insert into comments(comment, item_id) values ('comment1', 1);
+insert into comments(comment, item_id) values ('comment2', 2);
+insert into comments(comment, item_id) values ('comment3', 3);
+insert into comments(comment, item_id) values ('comment4', 3);
+insert into comments(comment, item_id) values ('comment5', 4);
+
+insert into attachs(path, item_id) values ('path1', 2);
+insert into attachs(path, item_id) values ('path2', 3);
+
+

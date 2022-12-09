1bf6e792ecfd39b32f5d87ce4c21f293a8702818
Stepan Bogdanov <bogstepan@gmail.com>
2022-12-09 23:16:03 +0300

    2. Парковка машин [#853 #367738]

--- src/main/java/ru/job4j/ood/lsp/parking/store/SimpleParking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/store/SimpleParking.java
@@ -3,76 +3,58 @@ package ru.job4j.ood.lsp.parking.store;
 import ru.job4j.ood.lsp.parking.model.Car;
 import static ru.job4j.ood.lsp.parking.model.PassengerCar.SIZE;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class SimpleParking implements Parking {
 
     private int pasCarFreePlaces;
     private int truckFreePlaces;
-    private List<Car> pasCarParking;
-    private List<Car> truckParking;
+    private Set<Car> pasCarParking;
+    private Set<Car> truckParking;
 
     public SimpleParking(int pasCarPlaces, int truckPlaces) {
         this.pasCarFreePlaces = pasCarPlaces;
         this.truckFreePlaces = truckPlaces;
-        pasCarParking = new ArrayList<>(pasCarPlaces);
-        truckParking = new ArrayList<>(truckPlaces);
+        pasCarParking = new HashSet<>(pasCarPlaces);
+        truckParking = new HashSet<>(truckPlaces);
     }
 
     @Override
     public boolean add(Car car) {
-        boolean rsl = false;
-        if (isPassengerCar(car)) {
-            if (pasCarFreePlaces > 0) {
-                pasCarParking.add(car);
-                pasCarFreePlaces--;
-                rsl = true;
-            }
-        } else {
-            if (truckFreePlaces > 0) {
-                truckParking.add(car);
-                truckFreePlaces--;
-                rsl = true;
-            } else if (pasCarFreePlaces >= car.getSize()) {
-                pasCarParking.add(car);
-                pasCarFreePlaces = pasCarFreePlaces - car.getSize();
-                rsl = true;
-            }
+        if (isPassengerCar(car) && pasCarFreePlaces >= 1) {
+            pasCarParking.add(car);
+            pasCarFreePlaces--;
+            return true;
         }
-        return rsl;
+        if (!isPassengerCar(car) && truckFreePlaces >= 1) {
+            truckParking.add(car);
+            truckFreePlaces--;
+            return true;
+        }
+        if (!isPassengerCar(car) && pasCarFreePlaces >= car.getSize()) {
+            pasCarParking.add(car);
+            pasCarFreePlaces = pasCarFreePlaces - car.getSize();
+            return true;
+        }
+        return false;
     }
 
     @Override
     public boolean delete(Car car) {
-        boolean rsl = false;
-        if (isPassengerCar(car)) {
-            for (Car pasCar : pasCarParking) {
-                if (pasCar.equals(car)) {
-                    pasCarParking.remove(car);
-                    rsl = true;
-                    break;
-                }
-            }
-        } else {
-            for (Car truck : truckParking) {
-                if (truck.equals(car)) {
-                    truckParking.remove(car);
-                    rsl = true;
-                    break;
-                }
-            }
-            if (!rsl) {
-                for (Car truck : pasCarParking) {
-                    if (truck.equals(car)) {
-                        pasCarParking.remove(car);
-                        rsl = true;
-                        break;
-                    }
-                }
-            }
+        if (isPassengerCar(car) && pasCarParking.contains(car)) {
+            pasCarParking.remove(car);
+            return true;
         }
-        return rsl;
+        if (!isPassengerCar(car) && truckParking.contains(car)) {
+            truckParking.remove(car);
+            return true;
+        }
+        if (!isPassengerCar(car) && pasCarParking.contains(car)) {
+            pasCarParking.remove(car);
+            return true;
+        }
+        return false;
     }
 
     private boolean isPassengerCar(Car car) {

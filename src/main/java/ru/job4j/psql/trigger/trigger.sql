3989cf116b8a74fe67adfe8609923f398919af9f
Stepan Bogdanov <bogstepan@gmail.com>
2023-01-20 11:30:51 +0300

    2. Триггеры [#504803 #344077]

--- src/main/java/ru/job4j/psql/trigger/trigger.sql
+++ src/main/java/ru/job4j/psql/trigger/trigger.sql
@@ -1,13 +1,47 @@
+create or replace function row_tax()
+	returns trigger as
+$$
+	BEGIN
+		update products
+		new.price = new.price + new.price * 0.2
+		return NEW;
+	END;
+$$
+LANGUAGE 'plpgsql';
+
 create trigger add_tax_before_insert
 before insert
 on products
 for each row
 execute procedure row_tax();
 
+create or replace function log_prices()
+	returns trigger as
+$$
+	BEGIN
+		insert into history_of_price (name, price, date)
+		values (NEW.name, NEW.price, now());
+		return NEW;
+	END
+$$
+LANGUAGE 'plpgsql'
+
 create trigger add_history
 before insert on products
 for each row
 execute procedure log_prices();
+
+create or replace function statement_tax()
+	returns trigger as
+$$
+	BEGIN
+		update products
+		set price = price + price * 0.2
+		where id = select id from interested;
+		return NEW;
+	END;
+$$
+LANGUAGE 'plpgsql';
 
 create trigger add_tax_after_insert
 after insert on products
